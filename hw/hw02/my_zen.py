c88f4b5bbb812d55e9f77ca216ce0b4c59f518c6
Liubomyr Halamaha <galamagal86@gmail.com>
2023-09-18 19:16:09 +0300

    added task zen

---
+++ hw/hw02/my_zen.py
@@ -0,0 +1,58 @@
+The Zen of Python, by Tim Peters
+
+1 - Yevhen Malyna, Capylyn
+2 - Anna Poshyta asanasa
+3 - Markiian Dmytruk mrknd
+4 - Aleksandr Shevchenko, Alexandr1123212545
+5 - Kostiantyn Yasinskyi YasinKostya
+6 - Tetiana Lesiv tatelesiv
+7 - Valeria Larchyk - LachykV
+8 - Karyna Bohoslovets Karina0615
+9 - Yulianna Drok Yulianna22
+10 - Oleg Krupyak, Krupyak
+11 - Matvii Lukin - Cawa123qwe
+12 - Olha Moroz olha-moroz
+13 - Dmytro Shpak, shpakdmitriy
+14 - Maksym Shylov , shyma66
+15 - MorpheusFR Kirill Suprun
+16 - YuriiGridin , Iurii Gridin
+17 - PavloHavron Pavlo Havron
+18 - Р—Р°РґРµРІСЃСЊРєР° Р”С–Р°РЅР° IDiana-Hub
+19 - РћР»РµРєСЃР°РЅРґСЂ Рљ SawaAser
+20 - shtvlad, Vladislav Shtanhei
+21 - andrii2323
+22 - Nadiia Illiushyna, somniumtx
+23 - Spectrvq (Maksym Oposhnii)
+24 - user01
+25 - user02
+26 - user03
+27 - user04
+28 - user05
+29 - user06
+30 - user07
+31 - user08
+32 - Iryna Iasko - nickname:  Irarasta
+33 - Yevhen Malyna, Capylyn
+34 - Anna Poshyta asanasa
+35 - Markiian Dmytruk mrknd
+36 - Aleksandr Shevchenko, Alexandr1123212545
+37 - Kostiantyn Yasinskyi YasinKostya
+38 - Tetiana Lesiv tatelesiv
+39 - Valeria Larchyk - LachykV
+40 - Karyna Bohoslovets Karina0615
+41 - Yulianna Drok Yulianna22
+42 - Oleg Krupyak, Krupyak
+43 - Matvii Lukin - Cawa123qwe
+44 - Olha Moroz olha-moroz
+45 - Dmytro Shpak, shpakdmitriy
+46 - Maksym Shylov , shyma66
+47 - MorpheusFR Kirill Suprun
+48 - YuriiGridin , Iurii Gridin
+49 - PavloHavron Pavlo Havron
+50 - Р—Р°РґРµРІСЃСЊРєР° Р”С–Р°РЅР° IDiana-Hub
+51 - РћР»РµРєСЃР°РЅРґСЂ Рљ SawaAser
+52 - shtvlad, Vladislav Shtanhei
+53 - andrii2323
+54 - Nadiia Illiushyna, somniumtx
+55 - Spectrvq (Maksym Oposhnii)
+56 - user01
