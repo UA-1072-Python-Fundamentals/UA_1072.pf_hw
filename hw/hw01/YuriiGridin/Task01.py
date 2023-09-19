e4449af091d4d00a58d1b08a2a35921dcd1a9b29
Yurii Gridin <g.gridini@gmail.com>
2023-09-19 13:49:49 +0300

    added hw01

---
+++ hw/hw01/YuriiGridin/Task01.py
@@ -0,0 +1,9 @@
+# Task01
+
+name = input("What is your name? ")
+age = input("How old are you? ")
+city = input("You live in? ")
+
+print("Hello,", name)
+print("Your age is", age)
+print("You live in", city)
---
+++ hw/hw01/YuriiGridin/Task02.py
@@ -0,0 +1,12 @@
+# Task02
+
+a = int(input("Enter the value A: "))
+b = int(input("Enter the value B: "))
+
+print("A+B= ", a + b)
+print("A-B= ", a - b)
+print("A*B= ", a * b)
+print("A/B= ", a / b)
+print("A**B= ", a ** b)
+print("A//B= ", a // b)
+print("A%B = ", a % b)
