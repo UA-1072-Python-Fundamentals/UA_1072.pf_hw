de7db9383b949ad8b8ca3d530787984fb9da268d
Iryna Iasko <145124857+Irarasta@users.noreply.github.com>
2023-09-20 15:06:26 +0300

    Add files via upload

---
+++ Irarasta_HV-Lesson_1.py
@@ -0,0 +1,69 @@
+#  # Task -1, question/reply
+#  # Get user input
+# name = input("WHAT IS YOUR NAME?")
+# age = input("HOW OLD ARE YOU? ")
+# city = input("WHERE DO YOU LIVE? ")
+#
+#  # Display messages
+# print(f"HELLO, {name}")
+# print(f"YOUR AGE IS {age}")
+# print(f"YOU LIVE IN {city}")
+
+
+
+
+
+#  #Task-2, math calculations
+#  #Get the users input info
+# A  = float(input('A =  '))
+# B = float(input('B =  '))
+#
+#  #Do the calculations
+# result_addition = A + B
+# result_substraction = A - B
+# result_multiplication = A * B
+# result_division = A / B
+# result_exponentation = A ** B
+# result_floor_division = A // B
+# result_modulus = A % B
+#
+#  #Output the results
+# print('Basic calculation for A and B are below: ')
+# print(f'{A} + {B} = {result_addition}')
+# print(f'{A} - {B} = {result_substraction}')
+# print(f'{A} * {B} = {result_multiplication}')
+# print(f'{A} / {B} = {result_division}')
+# print(f'{A} ** {B} = {result_exponentation}')
+# print(f'{A} // {B} = {result_floor_division}')
+# print(f'{A} % {B} = {result_modulus}')
+
+
+
+
+# # Task-2-another reading
+# # get users input
+# A = float(input("A = "))
+# B = float(input("B = "))
+#
+# add_result = A + B
+# substr_result = A - B
+# multipl_result = A * B
+# divis_result = A / B
+# exponent_result = A ** B
+# floor_divis_result = A // B
+# modulus_result = A % B
+#
+# print(f"A + B = {add_result}")
+# print(f"A - B = {substr_result}")
+# print(f"A * B = {multipl_result}")
+# print(f"A / B = {divis_result}")
+# print(f"A ** B = {exponent_result}")
+# print(f"A // B = {floor_divis_result}")
+# print(f"A % B = {modulus_result}")
+
+
+
+
+
+
+
