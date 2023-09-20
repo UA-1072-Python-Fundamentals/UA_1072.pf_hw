fab257f91e388cfccbada28bad5f1e543b43242a
platon <karyna.fedorenko@gmail.com>
2023-09-20 13:35:40 +0300

    Zen task update

--- hw/hw02/my_zen.py
+++ hw/hw02/my_zen.py
@@ -7,7 +7,7 @@ The Zen of Python, by Tim Peters
 5 - Kostiantyn Yasinskyi YasinKostya
 6 - Tetiana Lesiv tatelesiv
 7 - Valeria Larchyk - LachykV
-8 - Karyna Bohoslovets Karina0615
+8 - Special cases aren't special enough to break the rules.
 9 - Yulianna Drok Yulianna22
 10 - Oleg Krupyak, Krupyak
 11 - Matvii Lukin - Cawa123qwe
@@ -39,7 +39,7 @@ The Zen of Python, by Tim Peters
 37 - Kostiantyn Yasinskyi YasinKostya
 38 - Tetiana Lesiv tatelesiv
 39 - Valeria Larchyk - LachykV
-40 - Karyna Bohoslovets Karina0615
+40 - Simple is better than complex 
 41 - Yulianna Drok Yulianna22
 42 - Oleg Krupyak, Krupyak
 43 - Matvii Lukin - Cawa123qwe
