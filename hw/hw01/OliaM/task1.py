d8bff1b1f6d5ce9e82b25f4a66d9e6845d37546f
olha-moroz <helga.lettres@gmail.com>
2023-09-21 19:14:05 +0300

    try to add hw1

--- hw/hw01/OliaM/task1.py
+++
@@ -1,7 +0,0 @@
-name = str(input("WHAT IS YOUR NAME?"))
-age = int(input("HOW OLD ARE YOU?"))
-location = str(input("WHERE DO YOU LIVE?"))
-
-print("HELLO", name)
-print("YOUR AGE IS", age)
-print("YOU LIVE IN", location)
