5786c2a50187824a1847297ddbe35aff0c014f90
mrknd <markiian.design@gmail.com>
2023-09-22 11:00:46 +0200

    fix

---
+++ hw/hw01/Irarasta/Irarasta_HV-Lesson_1-task2-2.py
@@ -0,0 +1,34 @@
+ # Task-2-another variant of output representation
+
+
+ # get users input
+A = float(input(" Think of the number A = "))
+B = float(input("Now think of the number B = "))
+
+ # do the calculations
+add_result = A + B
+substr_result = A - B
+multipl_result = A * B
+divis_result = A / B
+exponent_result = A ** B
+floor_divis_result = A // B
+modulus_result = A % B
+
+ # output the result
+print('Here are some basic math operation for A and B: ')
+print(f"A + B = {add_result}")
+print(f"A - B = {substr_result}")
+print(f"A * B = {multipl_result}")
+print(f"A / B = {divis_result}")
+print(f"A ** B = {exponent_result}")
+print(f"A // B = {floor_divis_result}")
+print(f"A % B = {modulus_result}")
+
+
+
+
+
+
+
+
+
