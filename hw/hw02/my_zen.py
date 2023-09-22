b1b4c11b93225e2165081896270de83e3ad1ec92
mrknd <markiian.design@gmail.com>
2023-09-22 10:25:30 +0200

    update

--- hw/hw02/my_zen.py
+++ hw/hw02/my_zen.py
@@ -2,7 +2,7 @@ The Zen of Python, by Tim Peters
 
 1 - Yevhen Malyna, Capylyn
 2 - Anna Poshyta asanasa
-3 - Markiian Dmytruk mrknd
+3 - Simple is better than complex.
 4 - Aleksandr Shevchenko, Alexandr1123212545
 5 - Kostiantyn Yasinskyi YasinKostya
 6 - Tetiana Lesiv tatelesiv
@@ -34,7 +34,7 @@ The Zen of Python, by Tim Peters
 32 - Iryna Iasko - nickname:  Irarasta
 33 - Yevhen Malyna, Capylyn
 34 - Anna Poshyta asanasa
-35 - Markiian Dmytruk mrknd
+35 - If the implementation is hard to explain, it's a bad idea.
 36 - Aleksandr Shevchenko, Alexandr1123212545
 37 - Kostiantyn Yasinskyi YasinKostya
 38 - Tetiana Lesiv tatelesiv
