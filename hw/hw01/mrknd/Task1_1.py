f1812fcbf1ff6379087aaaaf3b5d086afda09d32
mrknd <markiian.design@gmail.com>
2023-09-22 14:51:52 +0200

    added t1

---
+++ hw/hw01/mrknd/Task1_1.py
@@ -0,0 +1,7 @@
+name = input("Enter a name: ")
+age = input("Enter how old are you: ")
+place = input("Enter where do you live: ")
+
+print("Hello", name)
+print("Your age is:", age)
+print("You live in", place)
