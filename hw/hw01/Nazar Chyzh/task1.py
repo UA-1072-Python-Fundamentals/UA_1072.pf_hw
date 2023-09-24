f1c929cbc162326f1fa7b2e634c9683eb9c91b72
Nazar Chyzh <nazarchyzhet@gmail.com>
2023-09-24 03:58:32 +0300

    added hw folders

---
+++ hw/hw01/Nazar Chyzh/task1.py
@@ -0,0 +1,9 @@
+name = input('What is your name?:')
+print("Hello, " + name) 
+
+age = input('How old you?:') 
+print("Your age is " + age)
+
+city = input('Where do you live?:')
+print("You live in " + city)
+
