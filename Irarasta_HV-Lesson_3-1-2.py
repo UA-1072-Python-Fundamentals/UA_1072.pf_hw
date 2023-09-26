6f9deb40bc56932fb2cc258e6e308ad9a64afc2f
Iryna Iasko <145124857+Irarasta@users.noreply.github.com>
2023-09-26 13:29:57 +0300

    Update Irarasta_HV-Lesson_3-1-2.py

--- Irarasta_HV-Lesson_3-1-2.py
+++ Irarasta_HV-Lesson_3-1-2.py
@@ -1,27 +1,3 @@
-#   ## Input a four-digit number:
-# num = int(input("Give me  a four-digit number: "))
-#
-# if 1000 <= num and num <= 9999:
-#     print('So, here it is... ')
-#   ## Find the product of the given digits:
-#     digit_1 = (num // 1000) % 10
-#     digit_2 = (num // 100) % 10
-#     digit_3 = (num // 10) % 10
-#     digit_4 = (num % 10)
-#     product = digit_1 * digit_2 * digit_3 * digit_4
-#
-#
-#   ## Write the given number in reverse order:
-#     reverse_num = digit_1 + digit_2 * 10 + digit_3 * 100 + digit_4 *1000
-#   ## Write the given  number in ascending order:
-#     ascending_order = ''.join(sorted(str(num)))
-#   ## Output the results:
-#     print(f"Product of digits = {product}")
-#     print(f"Number in reverse order - {reverse_num}")
-#     print(f"Number in ascending order - {ascending_order}")
-#   ## Check if the input is a four-digit number
-# else:
-#     print("Please, give me a  valid four-digit number.")
 
    ##BELOW IS THE BEST VARIANT
    ## Get input from the User
