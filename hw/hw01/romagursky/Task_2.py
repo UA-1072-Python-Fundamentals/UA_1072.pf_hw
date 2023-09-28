2bab80bf20b34e55538c6aa46b0d70799fc12d06
Roma Gursky <romangursky02@gmail.com>
2023-09-28 12:37:27 +0200

    Updated

--- hw/hw01/romagursky/Task_2.py
+++
@@ -1,8 +0,0 @@
-
-# TASK №2
-name = input("WHAT IS YOUR NAME?")
-age = input('HOW OLD ARE YOU?')
-city = input('WHERE DO YOU LIVE?')
-print("HElLO,"+ name)
-print('YOU AGE IS:' + age)
-print("YOU LIVE IN:" + city)
