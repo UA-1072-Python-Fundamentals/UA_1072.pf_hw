808013dec3480eaa68cec41ebeddeda0782bd0cb
“YasinKostya“ <kot201089@gmail.com>
2023-09-29 21:52:15 +0300

    Home Work 4_3

--- hw/hw04/YasinKostya/HW4.py
+++ hw/hw04/YasinKostya/HW4.py
@@ -1,11 +1,12 @@
 
-def convert_celsius (celsius: float):
-    ABSOLUTE_ZERO = -273.15
-    celsius = float(input("Enter the temperature in Celsius: "))
-    if celsius < ABSOLUTE_ZERO:
+def convert_celsius(celsius: float):
+    absolute_zero = -273.15
+    celsius_inp = float(input("Enter the temperature in Celsius: "))
+    if celsius_inp < absolute_zero:
         return "Error!!\nTemperature below absolute zero (-273.15°C)"
     else:
-        fahrenheit = (celsius * 9 / 5) + 32
-        return f"{celsius}°C is equivalent to {fahrenheit}°F"
-celsius = None
-print(convert_celsius(celsius))
+        fahrenheit = (celsius_inp * 9 / 5) + 32
+        return f"{celsius_inp}°C is equivalent to {fahrenheit}°F"
+celsius = float()
+# print(convert_celsius(celsius))
+convert_celsius(celsius)
