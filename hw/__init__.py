995f4505957ee01c189c55644ba1879aed8984c7
Oleg Krupyak <okrupyak@gmail.com>
2023-09-29 15:30:07 +0300

    Task lesson 4

---
+++ hw/__init__.py
@@ -0,0 +1,5 @@
+a = "John"
+b = "Bill"
+c = "Sean"
+default_order = f"Hello {a}, {b} and {c}"
+print(default_order)
---
+++ hw/hw03/__init__.py
@@ -0,0 +1,8 @@
+a = int(input("enter number a: "))
+b = int(input("enter number b: "))
+
+
+if a > b:
+    print("a is greater than b")
+elif a < b:
+    print("a is less than b")
