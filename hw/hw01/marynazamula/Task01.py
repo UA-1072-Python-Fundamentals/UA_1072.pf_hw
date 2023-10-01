90d9229be946a8605e558886ab4d67855a181f6e
marynazamula <marynazamula10@gmail.com>
2023-10-01 19:28:38 +0300

    added  task1

--- hw/hw01/marynazamula/Task01.py
+++
@@ -1,8 +0,0 @@
-print("Hello!")
-name =  input("What's your name?\n")
-age = input("How old are you?\n")
-city = input("Where do  you live?\n")
-print()
-print("Hello,", name)
-print("Your age:", age)
-print("You live in", city)
