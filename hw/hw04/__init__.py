4b5dde15725dff499b23eb1b580fddc2d2de8160
Oleg Krupyak <okrupyak@gmail.com>
2023-10-02 16:16:10 +0300

    Task lesson 4

---
+++ hw/hw04/__init__.py
@@ -0,0 +1,7 @@
+num1 = int(input("Enter a number k: "))
+num2 = int(input("Enter a number n: "))
+def k_to_k(num1, num2):
+    if num1 ** num1 == num2:
+        return True
+    else:
+        return False
