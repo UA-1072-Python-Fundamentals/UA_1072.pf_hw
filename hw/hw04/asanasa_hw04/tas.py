1b9433397a7a6fb742955f412634b7fb573ed79b
Anna Poshyta <anna.poshyta@nure.ua>
2023-10-04 15:04:51 +0300

    hw05 add

---
+++ hw/hw04/asanasa_hw04/tas.py
@@ -0,0 +1,19 @@
+number = 0
+while number <= 100:
+    if number %2 == 0:
+        print(number)
+    number += 1
+
+number = 0
+while number <= 100:
+    if number %2 != 0:
+        print(number)
+    number += 1
+
+l = [1,5,4,6,9,77]
+for i in l:
+    if not l %2 == 0:
+
+        print(l)
+
+
