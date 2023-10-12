2e8789345f4043d73064d14272c7a44b0897fe78
marynazamula <marynazamula10@gmail.com>
2023-10-12 16:07:50 +0300

    added task02

---
+++ hw/hw01/marynazamula/Task02.py
@@ -0,0 +1,7 @@
+name = input("What`s your name?\n")
+age = input("How old are you?\n")
+city = input("Where do you live?\n")
+
+print("Hello!", name)
+print("You age is", age)
+print("You live in", city)
