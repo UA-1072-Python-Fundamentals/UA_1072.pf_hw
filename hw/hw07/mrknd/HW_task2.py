53907c4975e20dffd9cbb00b84a02688efc3e99b
mrknd <markiian.design@gmail.com>
2023-10-15 21:58:42 +0200

    update

--- hw/hw07/mrknd/HW_task2.py
+++ hw/hw07/mrknd/HW_task2.py
@@ -17,21 +17,26 @@ def area_circle(r):
 
 
 def result():
-    ask_area = input("Enter the area you want to find(rectangle, triangle, circle): ").lower()
+    while True:
+        ask_area = input("Enter the area you want to find [rectangle, triangle, circle],"
+                         " or 'exit' to finish the program: ").lower()
 
-    if ask_area == "triangle":
-        ask_base = float(input("Enter the base: "))
-        ask_height = float(input("Enter the height: "))
-        return f"Area of triangle is {area_triangle(ask_base, ask_height)}"
+        if ask_area == 'exit':
+            break
 
-    if ask_area == "rectangle":
-        ask_length = float(input("Enter the lenght: "))
-        ask_width = float(input("Enter the width: "))
-        return f'Area of rectangle is {area_recrangle(ask_length, ask_width)}'
+        if ask_area == "triangle":
+            ask_base = float(input("Enter the base: "))
+            ask_height = float(input("Enter the height: "))
+            print(f"Area of triangle is {area_triangle(ask_base, ask_height)}")
 
-    if ask_area == "circle":
-        ask_radius = float(input("Enter the radius: "))
-        return f"Area of circle is {area_circle(ask_radius)}"
+        if ask_area == "rectangle":
+            ask_length = float(input("Enter the lenght: "))
+            ask_width = float(input("Enter the width: "))
+            print(f'Area of rectangle is {area_recrangle(ask_length, ask_width)}')
+
+        if ask_area == "circle":
+            ask_radius = float(input("Enter the radius: "))
+            print(f"Area of circle is {area_circle(ask_radius)}")
 
 
-print(result())
+result()
