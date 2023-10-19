2c148851880eaee616d388da4ef75a8db4530352
Andrii <andryha8653@gmail.com>
2023-10-19 21:26:24 +0300

    Revert "added hw05"
    This reverts commit c5d2dbd5776783b3c84e7a4b6e76f11469e7f3f0.

--- hw/hw05/andrii2323/task01.py
+++
@@ -1,7 +0,0 @@
-integer_list = [1, 2, 3, 4, 5]
-float_list = []
-for number in integer_list:
-  float_number = float(number)
-  float_list.append(float_number)
-
-print(float_list)
--- hw/hw05/andrii2323/task02.py
+++
@@ -1,9 +0,0 @@
-n = int(input("Enter a number: "))
-a, b = 0, 1
-print(a)
-print(b)
-while a + b <= n:
-    c = a + b
-    if c <= n:
-      print(c)
-    a, b = b, c
