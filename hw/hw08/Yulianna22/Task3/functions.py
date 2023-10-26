c9a1e111d718bad10648accde2e0fd8e73451c8a
Yulianna22 <yulianna.drok20@gmail.com>
2023-10-26 20:29:20 +0300

    added tasks2,3

---
+++ hw/hw08/Yulianna22/Task3/functions.py
@@ -0,0 +1,13 @@
+import math
+
+def rectangle_area(a,b):
+    s=a*b
+    return s
+
+def triangle_area(height,base):
+  s=0.5*height*base
+  return s
+
+def circle_area(radius):
+   s=math.pi*pow(radius,2)
+   return s  
