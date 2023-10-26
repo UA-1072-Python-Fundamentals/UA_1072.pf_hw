05ceb29dfff7103aa9be0acb5cd232c1ea2fb074
Iryna Iasko <145124857+Irarasta@users.noreply.github.com>
2023-10-26 11:41:15 +0300

    Update Irarasta_HV09-1-guess_game.py
    edited Guess Game

--- hw/hw09/Irarasta_HV09/Irarasta_HV09-1-guess_game.py
+++ hw/hw09/Irarasta_HV09/Irarasta_HV09-1-guess_game.py
@@ -1,23 +1,83 @@
   ## Guess game with 10 tries:
+import pygame
 import random
 
-gamehost_number = random.randint(1, 100) # Set guessing range 1 to 100
-max_tries = 10 # Set max for user`s tries
-users_tries = 0
+pygame.init()
 
-print('Lets play a Guess Game!\n'
-      'Guess the number between 1 and 100 in just 10 tries.')
+WIDTH, HEIGHT = 700, 300
+BACKGROUND_COLOR = (0, 0, 0)
+TEXT_COLOR = (255, 20, 147)
+FONT = pygame.font.Font(None, 36)
 
-while users_tries < max_tries:
-    guess = int(input('Guess my number: '))
-    users_tries += 1
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Guess Game")
 
-    if guess  < gamehost_number:
-        print('No, my number is greater.')
-    elif guess > gamehost_number:
-        print('No, my number is smaller.')
-    else:
-        print(f'Congrats! You win! \U0001F929 My guessed number is {guess}')
-        break
-if users_tries == max_tries:
-    print('You lose! I win! \U0001F61C.')
+TARGET_NUMBER = random.randint(1, 100)
+ATTEMPTS = 10
+attempts = 0
+guess = None
+message = ""
+attempt_counter = f"Attempts left: {ATTEMPTS}"
+your_guess = "Your guess: "
+
+running = True
+game_over = False
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        if not game_over:
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    running = False
+                elif pygame.K_0 <= event.key <= pygame.K_9:
+                    if attempts < ATTEMPTS:
+                        if guess is None:
+                            guess = 0
+                        guess = guess * 10 + (event.key - pygame.K_0)
+                elif event.key == pygame.K_RETURN:
+                    if attempts < ATTEMPTS:
+                        if guess is not None:
+                            if guess < TARGET_NUMBER:
+                                message = "No, my number is higher!"
+                            elif guess > TARGET_NUMBER:
+                                message = "No, my number is lower!"
+                            else:
+                                message = f"Congrats! You WIN! You`ve guessed the number ({TARGET_NUMBER})!"
+
+                                game_over = True
+                            attempts += 1
+                            guess = None
+                        if attempts >= ATTEMPTS and not game_over:
+                            message = f"Sorry, but you lose! I WIN! My number was {TARGET_NUMBER}."
+                            game_over = True
+                    attempt_counter = f"Attempts left: {ATTEMPTS - attempts}"
+
+                elif event.key == pygame.K_BACKSPACE:
+                    if guess is not None:
+                        guess //= 10
+
+    screen.fill(BACKGROUND_COLOR)
+
+    play_game_text = FONT.render("Play a Guess Game with me!", True, TEXT_COLOR)
+    screen.blit(play_game_text, (10, 10))
+
+    text = FONT.render(message, True, TEXT_COLOR)
+    screen.blit(text, (10, 50))
+
+    attempt_counter_text = FONT.render(attempt_counter, True, TEXT_COLOR)
+    screen.blit(attempt_counter_text, (10, 90))
+
+    guess_text = FONT.render(your_guess, True, TEXT_COLOR)
+    screen.blit(guess_text, (10, 130))
+
+    if not game_over:
+        guess_text = FONT.render(f"{guess if guess is not None else ''}", True, TEXT_COLOR)
+        screen.blit(guess_text, (160, 130))
+
+    pygame.display.flip()
+
+pygame.quit()
+
+
