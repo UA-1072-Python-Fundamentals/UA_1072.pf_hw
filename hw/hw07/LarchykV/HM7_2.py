4945ee086e736b862eaa8bf157551c7c3b5b90ca
LarchykV <vlarchyk@gmail.com>
2023-10-30 23:48:20 +0200

    fixed some issues with hw7

--- hw/hw07/LarchykV/HM7_2.py
+++ hw/hw07/LarchykV/HM7_2.py
@@ -1,19 +1,48 @@
-# Task 3
-input_word = input("Enter the word to calculate letters: ")
+# Task 2
 
 
-def char_calculator(word):
-    char_count = {}
-
-    for x in word:
-        if x in char_count:
-            char_count[x] += 1
-        else:
-            char_count[x] = 1
-    return char_count
+import math
 
 
-print("Result: ", char_calculator(input_word))
+def s_rectangle(a, b):
+    if a <= 0 or b <= 0:
+        return "impossible to calculate, all numbers must be greater than 0"
+    else:
+        return a * b
 
+
+def s_triangle(a, h):
+    if a <= 0 or h <= 0:
+        return "impossible to calculate, all numbers must be greater than 0"
+    else:
+        return 0.5 * a * h
+
+
+def s_circle(r):
+    if r <= 0:
+        return "impossible to calculate, all numbers must be greater than 0"
+    else:
+        return round(math.pi * r ** 2)
+
+
+def result():
+    area_type = input("Enter the area you'd like to find: rectangle, triangle or circle: ").lower()
+
+    if area_type == "rectangle":
+        length = float(input("Enter the length, please: "))
+        width = float(input("Enter the width, please: "))
+        print("The rectangle area is ", s_rectangle(length, width))
+
+    if area_type == "triangle":
+        base = float(input("Enter the base, please: "))
+        height = float(input("Enter the height, please: "))
+        print("The triangle area is ", (s_triangle(base, height)))
+
+    if area_type == "circle":
+        radius = float(input("Enter the radius, please: "))
+        print("The circle area is ", s_circle(radius))
+
+
+result()
 
 
--- hw/hw07/LarchykV/HW7_1.py
+++ hw/hw07/LarchykV/HW7_1.py
@@ -1,46 +1,25 @@
-#Task 2
-import math
+#Task 1
+def my_func():
+    """""
+    This function allows to enter two numbers and returns the larger of two.
 
+    """""
 
-def s_rectangle(a, b):
-    if a <= 0 or b <= 0:
-        return "impossible to calculate, all numbers must be greater than 0"
+    a = float(input("Please, enter the first number: "))
+    b = float(input("Please, enter the second number: "))
+
+    if a > b:
+        return a
+    elif a < b:
+        return b
     else:
-        return a * b
+        return "none, the numbers are equal."
+
+result = my_func()
+print(f"The larger number is: {result}")
 
 
-def s_triangle(a, h):
-    if a <= 0 or h <= 0:
-        return "impossible to calculate, all numbers must be greater than 0"
-    else:
-        return 0.5 * a * h
 
 
-def s_circle(r):
-    if r <= 0:
-        return "impossible to calculate, all numbers must be greater than 0"
-    else:
-        return round(math.pi * r**2)
-
-
-def result():
-    area_type = input("Enter the area you'd like to find: rectangle, triangle or circle: ").lower()
-
-    if area_type == "rectangle":
-        length = float(input("Enter the length, please: "))
-        width = float(input("Enter the width, please: "))
-        print("The rectangle area is ", s_rectangle(length, width))
-
-    if area_type == "triangle":
-        base = float(input("Enter the base, please: "))
-        height = float(input("Enter the height, please: "))
-        print("The triangle area is ", (s_triangle(base, height)))
-
-    if area_type == "circle":
-        radius = float(input("Enter the radius, please: "))
-        print("The circle area is ", s_circle(radius))
-
-
-result()
 
 
