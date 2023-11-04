5ce35990dc2b7a402cf213fcbbb4175c5f24819a
Diana Zalevska <zalevskaj.diana@gmail.com>
2023-11-04 21:07:48 +0200

    add hw8

---
+++ hw/hw08/DianaZ/task3/Area.py
@@ -0,0 +1,14 @@
+"""module for calculating the area of simple shapes"""
+from math import pow, pi
+
+
+def rectangle(a, b):
+    return round(a * b, 2)
+
+
+def triangle(a, h):
+    return round(0.5 * a * h, 2)
+
+
+def circle(r):
+    return round(pi * 2 * pow(r, 2), 2)
