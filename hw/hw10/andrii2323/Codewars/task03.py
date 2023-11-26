7043fcd79059946e31863d085b55a164ec9f5b12
Andrii <andryha8653@gmail.com>
2023-11-26 16:31:41 +0300

    added hw10

---
+++ hw/hw10/andrii2323/Codewars/task03.py
@@ -0,0 +1,13 @@
+class Human:
+    def __init__(self):
+        pass
+class Man(Human):
+    def __init__(self):
+        super().__init__()
+class Woman(Human):
+    def __init__(self):
+        super().__init__()
+
+def God():
+    #code
+    return [Man(), Woman()]
